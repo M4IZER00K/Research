1e0e51715b507859516f12d0ac89aaaa5a396065
Shashank Ganta <65840917+M4IZER00K@users.noreply.github.com>
2022-05-13 10:00:41 -0700

    Add files via upload

--- week2-MeasurementalDifference.py
+++ week2-MeasurementalDifference.py
@@ -66,9 +66,6 @@ for dataSet in dataList[2:]:
 #creates Dict for Protein : data 
 for Data in finalData:
     ProteinData[Data[0]] = Data[1:]
-#print(*finalData[0])
-#print(*ProteinData['HpCD00780223'])
-#calculate s2 for markers and average s2
 S2MetricData.pop(0)
 avg = 0.0
 count = 0.0
@@ -79,44 +76,49 @@ for Protein in week1Data:
     count += 1
 averageS2 = avg / count
 
+#create subset of markers that have s2 above average
 S2MetricProteinKey = {"test": float}
 S2MetricProteinKey.pop("test")
-#create subset of markers that have s2 above average
 for Protein in S2MetricData:
     if (Protein[1] > averageS2):
         subsetData.append(Protein[0])
         S2MetricProteinKey[Protein[0]] = Protein[1]
-#print(S2MetricProteinKey['HpCD00780232'])
 subsetData.pop(0)
-#print(*subsetData) 
+print(len(subsetData)) 
 
-
+#D_r_ has each protein with the case and the c value, while D_r_Cases has each protein with only the case
 D_r_ = {"test": []}
 D_r_.pop("test")
+D_r_Cases = {"test": []}
+D_r_Cases.pop("test")
 for Protein in subsetData:
     data = ProteinData[Protein]
     tempData = []
+    tempDataCases = []
     for point in data:
         if (float(point[0]) > 2 ):
             tempData.append(point)
+            tempDataCases.append(point[1:])
     D_r_[Protein] = tempData
-#print(*D_r_['HpCD00780232'])
+    D_r_Cases[Protein] = tempDataCases
+print(*D_r_Cases['HpCD00780232'])
 
+#given input of two Proteins, checks for similiarity of cases and depending on minimumSamples (variable to change) -> returns True if enough similar cases and False otherwise
 def Similiarity(Protein1, Protein2):
-    dataSet1 = D_r_[Protein1]
-    dataSet2 = D_r_[Protein2]
+    dataSet1 = D_r_Cases[Protein1]
+    dataSet2 = D_r_Cases[Protein2]
     sharedSamples = 0
     minimumSamples = 1
     for data in dataSet1:
+
         if data in dataSet2:
             sharedSamples += 1
     if sharedSamples >= minimumSamples:
         return True
     else:
         return False
-    #print(dataSet1)
-#print (R_r_("HpCD00780232","HpCD00780322"))
 
+#computes the S2 Metric for each case
 def compute(data):
     senstivity = 0.0
     specificity = 0.0
@@ -133,50 +135,40 @@ def compute(data):
                 specificity += 1
     p_Y1_D1 = senstivity/dPos
     p_Y0_D0 = specificity/dNeg
-    #print(p_Y0_D0)
-    #print(p_Y1_D1)
-    #print()
     return (p_Y1_D1*p_Y0_D0)
 
+#only finds the values for the first 500 out of ~1014 Proteins whose S2 Metric > avergage
 def conjunctive():
     R_r_ = {"test": []}
     notR_r_ = {"test": []}
     R_r_.pop("test")
     notR_r_.pop("test")
-    for Protein in subsetData:
+    for Protein in subsetData[:500]:
         tempProteinCorrelatedData = []
         notTempProteinCorrelatedData = []
         for SecondProtein in subsetData:
             if (Similiarity(Protein, SecondProtein) and Protein != SecondProtein):
                 dataset1 = ProteinData[Protein]
                 dataset2 = ProteinData[SecondProtein]
-                #print(dataset1[:50])
-                #print(dataset2[:50])
-                #print(Protein, "  ", SecondProtein)
                 tempSampleData = []
                 notTempSampleData = []
                 for index in range(len(dataset1)):
                     test = float(dataset1[index][0])
-                    #print(*dataset1[index])
-                    #print(*dataset2[index])
                     if (float(dataset1[index][0]) > 2 and float(dataset2[index][0]) > 2):
                         tempSampleData.append((1,dataset1[index][2]))
                     else:
                         tempSampleData.append((0, dataset1[index][2]))
                     if (float(dataset1[index][0]) <= 2 and float(dataset2[index][0]) > 2):
-                        #print(dataset1[index][0], "   ", dataset2[index][0])
                         notTempSampleData.append((1,dataset1[index][2]))
                     else:
                         notTempSampleData.append((0,dataset1[index][2]))
-                #print(tempSampleData[:50])
-                #print()
                 tempProteinCorrelatedData.append((SecondProtein,tempSampleData))
                 notTempProteinCorrelatedData.append((SecondProtein,notTempSampleData))
         if (len(tempProteinCorrelatedData) > 0):
             R_r_[Protein] = tempProteinCorrelatedData
         if (len(notTempProteinCorrelatedData) > 0):
             notR_r_[Protein] = notTempProteinCorrelatedData
-    #print(*R_r_)
+    #finds Eavg for the Protein
     for Protein in R_r_:
         eavg = 0
         for index in range(len(R_r_[Protein])):
@@ -184,44 +176,36 @@ def conjunctive():
             datanotP_r_r = notR_r_[Protein][index]
             Pr_r = compute(dataP_r_r[1])
             Pnotr_r = compute(datanotP_r_r[1])
-            #print(Pr_r, "   ", Pnotr_r)
             eavg += Pr_r - Pnotr_r
         eavg = eavg/len(R_r_[Protein])
-        #print(eavg)
         conjunctiveData[Protein] = eavg
 
+#only finds the values for the first 500 out of ~1014 Proteins whose S2 Metric > avergage
 def disjunctive():
     R_r_ = {"test": []}
     notR_r_ = {"test": []}
     R_r_.pop("test")
     notR_r_.pop("test")
-    for Protein in subsetData:
+    for Protein in subsetData[:500]:
         tempProteinCorrelatedData = []
         notTempProteinCorrelatedData = []
         for SecondProtein in subsetData:
             if (Similiarity(Protein, SecondProtein) and Protein != SecondProtein):
                 dataset1 = ProteinData[Protein]
                 dataset2 = ProteinData[SecondProtein]
-                #print(dataset1[:50])
-                #print(dataset2[:50])
                 print(Protein, "  ", SecondProtein)
                 tempSampleData = []
                 notTempSampleData = []
                 for index in range(len(dataset1)):
                     test = float(dataset1[index][0])
-                    #print(*dataset1[index])
-                    #print(*dataset2[index])
                     if (float(dataset1[index][0]) > 2 or float(dataset2[index][0]) > 2):
                         tempSampleData.append((1,dataset1[index][2]))
                     else:
                         tempSampleData.append((0, dataset1[index][2]))
                     if (float(dataset1[index][0]) <= 2 or float(dataset2[index][0]) > 2):
-                        #print(dataset1[index][0], "   ", dataset2[index][0])
                         notTempSampleData.append((1,dataset1[index][2]))
                     else:
                         notTempSampleData.append((0,dataset1[index][2]))
-                #print(tempSampleData[:50])
-                #print()
                 tempProteinCorrelatedData.append((SecondProtein,tempSampleData))
                 notTempProteinCorrelatedData.append((SecondProtein,notTempSampleData))
         if (len(tempProteinCorrelatedData) > 0):
@@ -235,10 +219,8 @@ def disjunctive():
             datanotP_r_r = notR_r_[Protein][index]
             Pr_r = compute(dataP_r_r[1])
             Pnotr_r = compute(datanotP_r_r[1])
-            #print(Pr_r, "   ", Pnotr_r)
             eavg += Pr_r - Pnotr_r
         eavg = eavg/len(R_r_[Protein])
-        #print(eavg)
         disjunctiveData[Protein] = eavg
     return disjunctiveData
 
@@ -247,9 +229,6 @@ def finalReturn():
 
     # "n/a" means that the specific Protein didnt have a value for conjunctive or disjunctive because it didn't share any common blood markers
     for Protein in S2Data:
-        #print(Protein)
-        #print(S2MetricProteinKey[Protein])
-        #print(*conjunctiveData)
         conjunctiveValue = ""
         disjunctiveValue = ""
         if Protein in conjunctiveData:
@@ -281,4 +260,3 @@ conjunctive()
 disjunctive()
 finalReturn()
 writeCSV()
-#print(*finalReturnData)
